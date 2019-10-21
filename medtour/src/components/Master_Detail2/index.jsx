1eacec065e585f7fcbbe6309f6b3ec703dff3866
huseyintaskesen <huseyintaskesen1@gmail.com>
2019-10-21 20:50:47 +0300

    patient and clinic profiles.
    no js. just html-css

--- medtour/src/components/Master_Detail2/index.jsx
+++ medtour/src/components/Master_Detail2/index.jsx
@@ -1,5 +1,12 @@
 ﻿import React, { Component } from "react";
 import ReactDOM from "react-dom";
+import { Icon, InlineIcon } from "@iconify/react";
+import home from "@iconify/icons-mdi/home";
+import doctorIcon from '@iconify/icons-mdi/doctor';
+import hospitalIcon from '@iconify/icons-mdi/hospital';
+import airplaneIcon from '@iconify/icons-mdi/airplane';
+import hotelIcon from '@iconify/icons-mdi/hotel';
+
 // import "./master-detail-2.css";
 
 export default class Master_Detail2 extends Component {
@@ -16,8 +23,8 @@ export default class Master_Detail2 extends Component {
                         <input
                             type="text"
                             className="form-control form-control-lg"
-                            placeholder="The treatment you are looking for?"
-                            aria-label="The treatment you are looking for?"
+                            placeholder="What type of treatment you are looking for?"
+                            aria-label="What type of treatment you are looking for?"
                             aria-describedby="button-addon2"
                         ></input>
                         <div className="input-group-append">
@@ -32,30 +39,26 @@ export default class Master_Detail2 extends Component {
                     </div>
                 </div>
                 <div className="buttons">
-                    <button className="btn btn-primary">
-                        <img
-                            alt="doctor-icon"
-                        ></img>
-                        &nbsp;&nbsp;&nbsp;&nbsp; Contracted Doctors
+                  <button className="btn btn-primary">
+                    <InlineIcon icon={doctorIcon} width="50" height="50" />
+                    <br/>
+                    &nbsp;&nbsp;&nbsp;&nbsp; Contracted Doctors                     
+                  </button>
+                    <button className="btn btn-primary">  
+                      <InlineIcon  icon={hospitalIcon} width="50" height="50"  />
+                      <br/>
+                      &nbsp;&nbsp;&nbsp;&nbsp; Explore Clinics
+                        
                     </button>
                     <button className="btn btn-primary">
-                        <img
-                            
-                            alt="clinic-icon"
-                        ></img>
-                        &nbsp;&nbsp;&nbsp;&nbsp; Explore Clinics
+                      <InlineIcon icon={airplaneIcon} width="50" height="50" />
+                      <br/>
+                      &nbsp;&nbsp;&nbsp;&nbsp; Look up for Flights
                     </button>
                     <button className="btn btn-primary">
-                        <img
-                            alt="flight-icon"
-                        ></img>
-                        &nbsp;&nbsp;&nbsp;&nbsp; Look up for Flights
-                    </button>
-                    <button className="btn btn-primary">
-                        <img
-                            alt="accomodation-icon"
-                        ></img>
-                        &nbsp;&nbsp;&nbsp;&nbsp; Find the best places to stay
+                      <InlineIcon icon={hotelIcon} width="50" height="50" />
+                      <br/>
+                      &nbsp;&nbsp;&nbsp;&nbsp; Find the best places to stay
                     </button>
                 </div>
             </div>
---
+++ medtour/src/components/ProfilePagePatientView/ProfilPagePatient.jsx
@@ -0,0 +1,82 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import ReactCrop from 'react-image-crop';
+import 'react-image-crop/dist/ReactCrop.css';
+
+export default function ProfilePagePatient () {
+    
+        return (
+            <div class="profile-page-container" >
+                <div class ="form-row" style={{marginBottom: '30px'}}>
+                <ReactCrop src="http://placekitten.com/200/300" />
+                </div>
+                
+                <form>
+                <div class="form-row">
+                    <div class="col-md-4 mb-3">
+                    <label for="validationDefault01">First name</label>
+                    <input type="text" class="form-control" id="validationDefault01" placeholder="First name" />
+                    </div>
+                    <div class="col-md-4 mb-3">
+                    <label for="validationDefault02">Last name</label>
+                    <input type="text" class="form-control" id="validationDefault02" placeholder="Last name" />
+                    </div>
+                    </div>
+                <div class="form-row">
+                    <div class="col-md-4 mb-3">                    
+                    <label for="validationDefaultUsername">New password</label>
+                    <div class="input-group">
+                        <div class="input-group-prepend">
+                        </div>
+                        <input type="text" class="form-control"  aria-describedby="inputGroupPrepend2" />
+                        </div>
+                    </div>
+                    <div class="col-md-4 mb-3">                    
+                    <label for="validationDefaultUsername">Re-enter new password</label>
+                    <div class="input-group">
+                        <div class="input-group-prepend">
+                        </div>
+                        <input type="text" class="form-control"   aria-describedby="inputGroupPrepend2" />
+                        </div>
+                    </div>
+                    
+                </div>   
+                
+                <div class="form-row">
+                    <div class="col-md-4 mb-3">                    
+                    <label for="validationDefaultUsername">Email address</label>
+                    <div class="input-group">
+                        <div class="input-group-prepend">
+                        </div>
+                        <input type="text" class="form-control"  placeholder="Email address" aria-describedby="inputGroupPrepend2" />
+                    </div>
+                    </div>
+
+                    <div class="col-md-4 mb-3">                    
+                    <label for="validationDefaultUsername">Mobile Phone</label>
+                    <div class="input-group">
+                        <div class="input-group-prepend">
+                        </div>
+                        <input type="text" class="form-control"  placeholder="" aria-describedby="inputGroupPrepend2" />
+                    </div>
+                    </div>
+                </div>
+                <div class="form-row">
+                    <div class="col-md-3 mb-3">
+                    <label for="validationDefault03">City</label>
+                    <input type="text" class="form-control" id="validationDefault03" placeholder="City" />
+                    </div>
+                    <div class="col-md-3 mb-3">
+                    <label for="validationDefault04">Country</label>
+                    <input type="text" class="form-control" id="validationDefault04" placeholder="Country" />
+                    </div>
+                    
+                </div>
+                <button class="btn btn-primary" type="submit">Update information</button>
+                </form>
+                 </div>
+                 
+                     
+        );
+    }
+  
