cf368bb82b80e0716a3e912516e43d9951880eac
huseyintaskesen <huseyintaskesen1@gmail.com>
2019-12-16 19:04:28 +0300

    body parser

--- medtour/server/app.js
+++ medtour/server/app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+const bodyParser = require("body-parser")
 
 const indexRouter = require("./routes/index");
 
@@ -16,6 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.resolve(__dirname, "build")));
+app.use(bodyParser.json());
 
 app.use("/api", indexRouter);
 app.get("*", (req, res) => {
