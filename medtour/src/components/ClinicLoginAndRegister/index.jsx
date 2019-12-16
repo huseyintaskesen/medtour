376f5e1ce3a9d598538e1c4c090d5bd456e20ab1
asafkaganbezgin <akb1996@hotmail.com>
2019-12-16 20:27:06 +0300

    Clinic
    Register and Login

--- medtour/src/components/ClinicLoginAndRegister/index.jsx
+++ medtour/src/components/ClinicLoginAndRegister/index.jsx
@@ -14,8 +14,79 @@ class ClinicLoginAndRegister extends Component {
         return (
             <div>
                 <LandingNav />
-                <div className="container-fluid vertical-center">
-                    <div className="col-6 offset-3 bg-white">Hello</div>
+                <div className="container-fluid">
+                    <div className="col-6 offset-3 w3-animate-opacity bg-white top">
+                        <div className="row pl-4">
+                            <div className="col-6">
+                                <h3 className="text-center txt-color">
+                                    Clinic Login
+                                </h3>
+                                <div className="row col-12">
+                                    <form className="col-12">
+                                        <div
+                                            className="form-group"
+                                            id="clinic-login"
+                                        >
+                                            <label>Clinic Email</label>
+                                            <input
+                                                type="email"
+                                                className="form-control"
+                                                placeholder="enter registered clinic email"
+                                            ></input>
+                                            <label>Password</label>
+                                            <input
+                                                type="password"
+                                                className="form-control"
+                                                placeholder="enter password"
+                                            ></input>
+                                        </div>
+                                    </form>
+                                    <div className="col-12 text-center pt-4">
+                                        <button className="btn btn-primary">
+                                            Login
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-6">
+                                <h3 className="text-center txt-color">
+                                    Clinic Register
+                                </h3>
+                                <div className="row col-12">
+                                    <form className="col-12">
+                                        <div
+                                            className="form-group"
+                                            id="clinic-register"
+                                        >
+                                            <label>Clinic Name</label>
+                                            <input
+                                                type="text"
+                                                className="form-control"
+                                                placeholder="enter your clinic name"
+                                            ></input>
+                                            <label>Clinic Email</label>
+                                            <input
+                                                type="email"
+                                                className="form-control"
+                                                placeholder="enter an email"
+                                            ></input>
+                                            <label>Password</label>
+                                            <input
+                                                type="password"
+                                                className="form-control"
+                                                placeholder="chose a password"
+                                            ></input>
+                                        </div>
+                                    </form>
+                                    <div className="col-12 text-center pt-4">
+                                        <button className="btn btn-primary">
+                                            Sign Up
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         );
