284f7c4f8e7b8a6d86124a3a40200900daf073d8
asafkaganbezgin <akb1996@hotmail.com>
2019-12-16 20:45:17 +0300

    Code Cleanup

--- medtour/src/App.jsx
+++ medtour/src/App.jsx
@@ -10,11 +10,7 @@ import Login from "./components/Login";
 import SignUp from "./components/SignUp";
 import ClinicLoginAndRegister from "./components/ClinicLoginAndRegister";
 import Contact from "./components/Contact";
-import Master_Detail from "./components/Master_Detail";
-import Master_Detail2 from "./components/Master_Detail2";
-import Master_Detail3 from "./components/Master_Detail3";
 import SearchResultsView from "./components/SearchResultsView";
-import Master_Detail5 from "./components/Master_Detail5";
 import Grid from "./components/Grid";
 import ProfilePagePatientView from "./components/ProfilePagePatientView";
 import ProfilePageClinic from "./components/ProfilePageClinicView/ProfilePageClinic";
@@ -35,14 +31,10 @@ class App extends Component {
                         component={ClinicLoginAndRegister}
                     />
                     <Route path="/Contact" component={Contact} />
-                    <Route path="/Master_Detail" component={Master_Detail} />
-                    <Route path="/Master_Detail2" component={Master_Detail2} />
-                    <Route path="/Master_Detail3" component={Master_Detail3} />
                     <Route
                         path="/SearchResultsView"
                         component={SearchResultsView}
                     />
-                    <Route path="/Master_Detail5" component={Master_Detail5} />
                     <Route path="/Grid" component={Grid} />
                     <Route path="/profile" component={ProfilePagePatientView} />
                     <Route
