ed47d711147e607f732b5e0a97cab130fc793a5f
asafkaganbezgin <akb1996@hotmail.com>
2019-12-24 23:52:41 +0300

    Bootstrap Added

--- medtour/src/index.jsx
+++ medtour/src/index.jsx
@@ -3,11 +3,8 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import registerServiceWorker from "./registerServiceWorker";
-<<<<<<< HEAD
 
-=======
 // import "bootstrap/dist/css/bootstrap.min.css";
->>>>>>> 8f6ec6b0a6697d12f811a7ad77ec67ad1b37dd87
 
 ReactDOM.render(
     <BrowserRouter>
