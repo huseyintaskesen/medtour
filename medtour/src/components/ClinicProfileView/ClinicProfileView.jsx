ae364bb3a93936f3177d4fe8f378a0b8c8a1352f
huseyintaskesen <huseyintaskesen1@gmail.com>
2019-12-26 22:34:12 +0300

    chat kit and clinic root view

--- medtour/src/components/ClinicProfileView/ClinicProfileView.jsx
+++ medtour/src/components/ClinicProfileView/ClinicProfileView.jsx
@@ -3,6 +3,7 @@ import './clinicprofileview.css';
 import Agenda from './components/agenda'
 import ButtonAppBar from './components/dropdown.js'
 import VirtualizedList from './components/treatmentList.js'
+import Settings from '../ClinicSettings'
 
 var c_id;
 
@@ -10,23 +11,67 @@ class ClinicProfileView extends Component {
   
   constructor(props){
     super(props)
+    this.state = {
+      currentView:'calendar'
+    }
+
+    this.changeView = this.changeView.bind(this);
+
+  }
+
+  changeView(view){
+    this.setState({
+      currentView: view
+    })
   }
   
 
   render() {
-    return (
-      <div className="ClinicProfileView bg-white">
-        <div>
-        <ButtonAppBar ></ButtonAppBar>
-        </div>
-        <div className="row">
-          <div className="column">
-          <Agenda ></Agenda>
+    let view =''
+    
+    if(this.state.currentView == 'calendar'){
+      return (
+
+        <div className="ClinicProfileView bg-white">
+          <div>
+          <ButtonAppBar changeView = {this.changeView}></ButtonAppBar>
+          </div>
+          <div className="row">
+            <div className="column">
+            <Agenda ></Agenda>
+            </div>
           </div>
         </div>
-      </div>
+  
+      );
+    }
 
-    );
+    if(this.state.currentView == 'messages'){
+      return (
+        <div >
+          <div>
+          <ButtonAppBar changeView = {this.changeView}></ButtonAppBar>
+          </div>
+          hello
+        </div>
+  
+      );
+    }
+
+    if(this.state.currentView == 'settings'){
+      return (
+        <div >
+          <div>
+          <ButtonAppBar changeView = {this.changeView}></ButtonAppBar>
+          </div>
+         <Settings></Settings>
+        </div>
+  
+      );
+    }
+
+
+    
   }
 }
 
