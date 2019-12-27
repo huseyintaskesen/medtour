236f9216196d6cea7c7bef5f4eb0340db9389f91
huseyintaskesen <huseyintaskesen1@gmail.com>
2019-12-27 05:26:51 +0300

    clinic profile

--- medtour/src/components/ClinicProfileView/ClinicProfileView.jsx
+++ medtour/src/components/ClinicProfileView/ClinicProfileView.jsx
@@ -13,11 +13,47 @@ class ClinicProfileView extends Component {
   constructor(props){
     super(props)
     this.state = {
-      currentView:'calendar'
+      currentView:'',
+      userName:'',
+      surName:'',
+      treatmentName:'',
+      treatmentPrice:0,
+      treatmentDate: new Date()
     }
 
     this.changeView = this.changeView.bind(this);
 
+  }
+
+
+  componentDidMount(){
+    c_id = localStorage.getItem('clinicID');
+       
+        const options = {
+            headers: {'content-type': 'application/json'}
+        };
+        // search/"+treatment_type
+        fetch("http://localhost:3001/api/tourData/clinic/"+c_id, {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        }).then(response => {
+            return response.json();
+        }).then(muutuja => {
+            console.log('RESPONSE'+muutuja.trans[0].u_id.name)
+            this.setState({
+                userName: muutuja.trans[0].u_id.name,
+                surName: muutuja.trans[0].u_id.surname,
+                treatmentName: muutuja.trans[0].t_id.name,
+                treatmentDate: muutuja.trans[0].treatment_Date,
+                treatmentPrice: muutuja.trans[0].t_id.priceLow
+            },()=>{
+              console.log(this.state)
+              this.setState({currentView: 'calendar'})
+            })
+            
+        });
   }
 
   changeView(view){
@@ -25,21 +61,31 @@ class ClinicProfileView extends Component {
       currentView: view
     })
   }
+
   
 
   render() {
     let view =''
-    
+    if(this.state.currentView == ''){
+      return (
+        <div className="ClinicProfileView bg-white">
+          <div>
+          <ButtonAppBar changeView = {this.changeView}></ButtonAppBar>
+          </div>
+          <div className="row">
+          </div>
+        </div>
+      );
+    }
     if(this.state.currentView == 'calendar'){
       return (
-
         <div className="ClinicProfileView bg-white">
           <div>
           <ButtonAppBar changeView = {this.changeView}></ButtonAppBar>
           </div>
           <div className="row">
             <div className="column">
-            <Agenda ></Agenda>
+            <Agenda treatment_object={this.state}></Agenda>
             </div>
           </div>
         </div>
